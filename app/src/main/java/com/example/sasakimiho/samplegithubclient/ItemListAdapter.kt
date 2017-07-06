28ccce93fd2dae9c0768308b1ae6d2228ca076c5
MihoSasaki <miho.sprit@gmail.com>
2017-07-06 18:25:11 +0900

    add implementation for recyclerview

--- app/src/main/java/com/example/sasakimiho/samplegithubclient/ItemListAdapter.kt
+++ app/src/main/java/com/example/sasakimiho/samplegithubclient/ItemListAdapter.kt
@@ -4,11 +4,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.RelativeLayout
 import android.widget.TextView
 import butterknife.bindView
+import com.bumptech.glide.Glide
 
-class ItemListAdapter(val items: List<String>, val itemClicked: (String) -> Unit) : RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
+class ItemListAdapter(val items: MutableList<Repository>, val itemClicked: (Repository) -> Unit) : RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ItemListAdapter.ViewHolder, position: Int) {
         holder.setUp(items[position])
     }
@@ -22,14 +25,58 @@ class ItemListAdapter(val items: List<String>, val itemClicked: (String) -> Unit
         return ViewHolder(view = view, itemClicked = itemClicked)
     }
 
-    class ViewHolder(view: View, val itemClicked: (String) -> Unit) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(val view: View, val itemClicked: (Repository) -> Unit) : RecyclerView.ViewHolder(view) {
         private val textView: TextView by bindView(R.id.repository_name)
+        private val nameView: TextView by bindView(R.id.user_name)
         private val eachList: RelativeLayout by bindView(R.id.each_item)
+        private val icon: ImageView by bindView(R.id.repository_icon)
+        private val ratingBar: RatingBar by bindView(R.id.rating_bar)
+        private val ratingNum: TextView by bindView(R.id.num_rating)
 
-        fun setUp(itemName: String) {
-            this.textView.text = itemName
-            this.eachList.setOnClickListener { itemClicked(itemName) }
+        fun setUp(repository: Repository) {
+            this.textView.text = repository.fullName
+            this.nameView.text = repository.owner.login
+            this.eachList.setOnClickListener { itemClicked(repository) }
+            this.ratingBar.rating = setRatingBarNum(repository.score)
+            this.ratingNum.text = repository.score.toString()
+            Glide.with(view.context).load(repository.owner.avatarUrl).into(this.icon)
+        }
+
+        fun setRatingBarNum(num: Float): Float {
+            if (num < 20.toFloat()) {
+                return 1.toFloat()
+            } else if (num >= 20.toFloat() && num < 40.toFloat()) {
+                return 2.toFloat()
+            } else if (num >= 40.toFloat() && num < 60.toFloat()) {
+                return 3.toFloat()
+            } else if (num >= 60.toFloat() && num < 80.toFloat()) {
+                return 4.toFloat()
+            } else if (num >= 80.toFloat()) {
+                return 5.toFloat()
+            }
+            return 0.toFloat()
         }
     }
 
+    fun addRepository(repository: Repository) {
+        synchronized(items) {
+            items.add(repository)
+            notifyItemInserted(items.size - 1)
+        }
+    }
+
+    fun addRepositories(repositories: List<Repository>) {
+        synchronized(items) {
+            val currentItemSize: Int = items.size
+            items.addAll(repositories)
+            notifyItemRangeInserted(currentItemSize, repositories.size)
+        }
+    }
+
+    fun refreshRepositories() {
+        synchronized(items) {
+            items.clear()
+            notifyDataSetChanged()
+        }
+    }
 }
--- app/src/main/java/com/example/sasakimiho/samplegithubclient/MainActivity.kt
+++ app/src/main/java/com/example/sasakimiho/samplegithubclient/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.sasakimiho.samplegithubclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.widget.Toast
+import butterknife.bindView
 
 class MainActivity : AppCompatActivity() {
+
+    private val recyclerView: RecyclerView by bindView(R.id.recycler_view)
+    private lateinit var searchView: SearchView
+
+    private var searchWord: String = ""
+
+    private val SORT_NONE_INDEX: Int = 3
+    private val ORDER_NONE_INDEX: Int = 2
+    private var sortSelectedId: Int = SORT_NONE_INDEX
+    private var orderSelectedId: Int = ORDER_NONE_INDEX
+
+    private var sortQuerySelected: SortQuery = SortQuery.NONE
+    private var orderQuerySelected: OrderQuery = OrderQuery.NONE
+
+    val itemList: MutableList <Repository> = mutableListOf<Repository>()
+    val adapter = ItemListAdapter(items = itemList) { repo ->
+        Toast.makeText(this, repo.description, Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.adapter = adapter
     }
 }
