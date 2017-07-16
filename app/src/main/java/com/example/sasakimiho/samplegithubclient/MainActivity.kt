69f57262e8daf9a52be58bd7e0111c0f037734fe
MihoSasaki <miho.sprit@gmail.com>
2017-07-16 11:17:37 +0900

    add implementation for calling api on github and options menu

--- app/src/main/java/com/example/sasakimiho/samplegithubclient/MainActivity.kt
+++ app/src/main/java/com/example/sasakimiho/samplegithubclient/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.sasakimiho.samplegithubclient
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.support.v4.view.MenuItemCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import butterknife.bindView
+import co.metalab.asyncawait.async
+import retrofit2.Response
+import ru.gildor.coroutines.retrofit.awaitResponse
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +41,153 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerView.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val menuItem: MenuItem = menu.findItem(R.id.search_view)
+        this.searchView = MenuItemCompat.getActionView(menuItem) as SearchView
+        this.searchView.setIconifiedByDefault(true)
+        this.searchView.isSubmitButtonEnabled = false
+
+        if (this.searchWord != "") {
+            this.searchView.setQuery(this.searchWord, false)
+        } else {
+            val queryHint: String = resources.getString(R.string.abc_search_hint)
+            this.searchView.queryHint = queryHint
+        }
+
+        this.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searchWord = newText as String
+                //TODO:future work
+                //textの結果をfragmentに送る
+                async {
+                    newText?.let { searchRepository(query = newText) }
+                }
+                return true
+            }
+        })
+
+        this.searchView.setOnQueryTextFocusChangeListener { view, b ->
+            if (b) {
+                //TODO:future work
+                //true->fragmentを生成(もしfragmentを使用する場合)
+                this.supportActionBar?.title = ""
+            } else {
+                this.supportActionBar?.title = getString(R.string.app_name)
+            }
+        }
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.options_sort_menu) {
+            sortItemClicked()
+        }
+        if (item.itemId == R.id.options_order_menu) {
+            orderItemClicked()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun sortItemClicked() {
+        val stringTitles: Array<String> = SortQuery.getAllQueries()
+        var currentSelectedId: Int = sortSelectedId
+        AlertDialog.Builder(this)
+                .setTitle("select sort item")
+                .setSingleChoiceItems(stringTitles, sortSelectedId, object : DialogInterface.OnClickListener {
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+                        currentSelectedId = p1
+                    }
+                })
+                .setPositiveButton("Select", object : DialogInterface.OnClickListener {
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+                        changeSortSelectedItem(stringTitles[currentSelectedId])
+                        sortSelectedId = currentSelectedId
+                        async {
+                            searchRepository(searchWord)
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show()
+    }
+
+    fun orderItemClicked() {
+        val stringTitles: Array<String> = OrderQuery.getAllQueries()
+        var currentSelectedId: Int = orderSelectedId
+        AlertDialog.Builder(this)
+                .setTitle("select order item")
+                .setSingleChoiceItems(stringTitles, orderSelectedId, object : DialogInterface.OnClickListener {
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+                        currentSelectedId = p1
+                    }
+                })
+                .setPositiveButton("Select", object : DialogInterface.OnClickListener {
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+                        changeOrderSelectedItem(stringTitles[currentSelectedId])
+                        orderSelectedId = currentSelectedId
+                        async {
+                            searchRepository(searchWord)
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show()
+    }
+
+    fun changeSortSelectedItem(item: String) {
+        when (item) {
+            SortQuery.FORK.query -> sortQuerySelected = SortQuery.FORK
+            SortQuery.STAR.query -> sortQuerySelected = SortQuery.STAR
+            SortQuery.UPDATED.query -> sortQuerySelected = SortQuery.UPDATED
+            SortQuery.NONE.query -> sortQuerySelected = SortQuery.NONE
+        }
+    }
+
+    fun changeOrderSelectedItem(item: String) {
+        when (item) {
+            OrderQuery.ASC.query -> orderQuerySelected = OrderQuery.ASC
+            OrderQuery.DESC.query -> orderQuerySelected = OrderQuery.DESC
+            OrderQuery.NONE.query -> orderQuerySelected = OrderQuery.NONE
+        }
+    }
+
+    suspend fun searchRepository(query: String) {
+        val queryMap: MutableMap<String, String> = mutableMapOf()
+        queryMap.put("q", query)
+        if (sortQuerySelected != SortQuery.NONE) {
+            queryMap.put("sort", sortQuerySelected.query)
+        }
+        if (orderQuerySelected != OrderQuery.NONE) {
+            queryMap.put("order", orderQuerySelected.query)
+        }
+
+        try {
+            val appModule: AppModule = AppModule()
+            val githubClient: GithubClient = appModule.providerGithubClient(appModule.provideRetrofit(appModule.providerGson(), appModule.getClient()))
+            val result: Response<Page<Repository>> = githubClient.search(queryMap).awaitResponse()
+            if (result.isSuccessful) {
+                //if successful
+                val repository: Repository? = result.body()?.items?.get(0)
+                println(result.body())
+                result.body()?.items?.let {
+                    adapter.refreshRepositories()
+                    adapter.addRepositories(it)
+                }
+            }
+        } catch (e: Throwable) {
+            //TODO: handle error!!
+            Log.e("api call", "is not successful" + e)
+        }
     }
 }
